8946ffada3e4db1aeba1e97bac814438b16ce52d
joselius16 <jlalvarezaldana@gmail.com>
2025-02-09 16:26:09 +0100

    added tests

---
+++ tests/test_connection.py
@@ -0,0 +1,19 @@
+from adapted_work.database.tables import Andalucia
+from sqlmodel import select
+
+
+
+def test_insert_andalucia(db_session):
+    """Prueba insertar y consultar datos en la tabla andalucia."""
+    url = Andalucia(id=1, url="test")
+    db_session.add(url)
+    db_session.commit()
+  
+
+
+def test_get_data(db_session):
+    query = select(Andalucia)
+    result = db_session.exec(query).first()
+    
+    assert result is not None
+
