8e6361d511f6745c860b78edc9516edd660b69a9
joselius16 <jlalvarezaldana@gmail.com>
2025-03-02 17:34:36 +0100

    added endpoint from aragon

--- adapted_work/api.py
+++ adapted_work/api.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 from adapted_work.router.comunities.andalucia.get_andalucia.rest import \
     router as get_all_andalucia
+from adapted_work.router.comunities.aragon.get_aragon.rest import \
+    router as get_all_aragon
 from adapted_work.router.comunities.extremadura.get_extremadura.rest import \
     router as get_all_extremadura
 
@@ -11,3 +13,4 @@ api_router.include_router(get_all_andalucia, tags=["Andalucia"], prefix="/andalu
 api_router.include_router(
     get_all_extremadura, tags=["Extremadura"], prefix="/extremadura"
 )
+api_router.include_router(get_all_aragon, tags=["Aragon"], prefix="/aragon")
