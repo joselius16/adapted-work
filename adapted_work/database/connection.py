df6dca11803e2b2f67ee8990c7a9d274d89d059f
joselius16 <jlalvarezaldana@gmail.com>
2025-03-17 19:54:35 +0100

    reorganized database

--- adapted_work/database/connection.py
+++ adapted_work/database/connection.py
@@ -1,13 +1,63 @@
-from sqlmodel import Session, SQLModel, create_engine
+from loguru import logger
+from sqlmodel import Session, SQLModel, create_engine, select, text
 
+from adapted_work.database.tables import (  # 🔹 Importa los modelos aquí
+    Comunity, ComunityType)
 from adapted_work.settings import settings
 
 engine = create_engine(settings.database_uri, echo=True)
 
-# Build table if doesn't exists
+
+def create_schema_if_not_exists(engine, schema=settings.schema_database):
+    with engine.connect() as conn:
+        conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema}"))
+        conn.commit()
+
+
+create_schema_if_not_exists(engine)
 SQLModel.metadata.create_all(engine)
 
 
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+def initialize_data():
+    """Check if data exists."""
+    try:
+        with Session(engine) as session:
+            existing = session.exec(
+                select(ComunityType).where(
+                    ComunityType.comunity_name.in_(
+                        ["Andalucia", "Aragon", "Extremadura", "Murcia"]
+                    )
+                )
+            ).all()
+
+            if not existing:
+                comunities = [
+                    ComunityType(
+                        comunity_name="Andalucia",
+                        url_comunity="https://www.juntadeandalucia.es/",
+                    ),
+                    ComunityType(
+                        comunity_name="Aragon",
+                        url_comunity="https://aplicaciones.aragon.es/",
+                    ),
+                    ComunityType(
+                        comunity_name="Extremadura",
+                        url_comunity="https://www.juntaex.es/",
+                    ),
+                    ComunityType(
+                        comunity_name="Murcia",
+                        url_comunity="https://empleopublico.carm.es/",
+                    ),
+                ]
+                session.add_all(comunities)
+                session.commit()
+                logger.info("Data inserted propertly.")
+            else:
+                logger.info("Data already inserted.")
+    except:
+        logger.error("Cannot execute initialize data.")
--- adapted_work/database/tables.py
+++ adapted_work/database/tables.py
@@ -2,9 +2,29 @@ from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
+from adapted_work.settings import settings, table_names
+
+
+class ComunityType(SQLModel, table=True):
+    __tablename__ = table_names.comunity_type
+    __table_args__ = {"schema": settings.schema_database}
+
+    id: int = Field(default=None, primary_key=True)
+    comunity_name: str
+    url_comunity: str
+
 
 class Comunity(SQLModel, table=True):
+    __tablename__ = table_names.comunity
+    __table_args__ = {"schema": settings.schema_database}
+
     id: int = Field(default=None, primary_key=True)
-    url: str
+    id_comunity_type: int = Field(
+        foreign_key=f"{settings.schema_database}.{table_names.comunity_type}.id"
+    )
+    ext_url: str
+    dates: Optional[str] = None
     disability_vacancies: Optional[str] = None
-    end_date: Optional[str] = None
+    specialty: Optional[str] = None
+    qualifycation: Optional[str] = None
+    type_of_personnel: Optional[str] = None
