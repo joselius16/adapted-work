43bbe2bd972888a97aca63422e995b43f2d7d819
joselius16 <jlalvarezaldana@gmail.com>
2025-06-05 19:25:41 +0200

    ollama model as variable

--- adapted_work/settings.py
+++ adapted_work/settings.py
@@ -8,13 +8,16 @@ class Settings(BaseSettings):
     log_path: str = Field("logs/webscraping.log", env="LOG_PATH")
     database_uri: str = Field(..., env="DATABASE_URI")
 
+    #Ollama model
+    ollama_model: str = Field(..., env="OLLAMA_MODEL")
+
     # Andalucia
     base_url_andalucia: str = Field(
         "https://www.juntadeandalucia.es/organismos/iaap/areas/empleo-publico/procesos-selectivos/detalle/",
         env="BASE_URL_ANDALUCIA",
     )
     start_id_andalucia: int = Field(514009, env="START_ID_ANDALUCIA")
-    end_id_andalucia: int = Field(514016, env="END_ID_ANDALUCIA")
+    end_id_andalucia: int = Field(514116, env="END_ID_ANDALUCIA")
 
     class Config:
         env_file = ".env"
