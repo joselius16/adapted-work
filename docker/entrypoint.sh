2e084c1e12836a2d4172971ed7680bc544f10c7f
joselius16 <jlalvarezaldana@gmail.com>
2025-06-05 19:20:38 +0200

    execute image with entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download Ollama Model
+OLLAMA_MODEL=${OLLAMA_MODEL}
+ollama pull $OLLAMA_MODEL
+
+# Executing API
+python -m uvicorn adapted_work.app:app --host 0.0.0.0 --port 8000 &
+sleep 5
+
+#Executing scheduler
+export PYTHONPATH=$(pwd)
+python adapted_work/scheduler/scheduler.py
