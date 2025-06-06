f335857571bd94fe07f420e1d34843fc34df30fd
joselius16 <jlalvarezaldana@gmail.com>
2025-06-06 19:10:57 +0200

    dockerfile with user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Initiate Ollama
+ollama serve
+
 # Download Ollama Model
 OLLAMA_MODEL=${OLLAMA_MODEL}
 ollama pull $OLLAMA_MODEL
@@ -8,6 +11,6 @@ ollama pull $OLLAMA_MODEL
 python -m uvicorn adapted_work.app:app --host 0.0.0.0 --port 8000 &
 sleep 5
 
-#Executing scheduler
+# Executing scheduler
 export PYTHONPATH=$(pwd)
 python adapted_work/scheduler/scheduler.py
